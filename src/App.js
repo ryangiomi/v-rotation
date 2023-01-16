d7caef1f18dabd49852cce26dd5bae122e2b46ce
pap <ryangiomi@gmail.com>
2023-01-16 20:04:07 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ import Rotation from "./Rotation";
 export default function App() {
   return (
     <div className="App">
-      <div
-        style={
-          {
-            // display: "grid",
-            // gridRowStart: 1,
-            // gridRowEnd: 13,
-            // gridColumnStart: 1,
-            // gridColumnEnd: 3,
-          }
-        }
-      >
-        <Rotation />
-      </div>
+      <Rotation />
     </div>
   );
 }
