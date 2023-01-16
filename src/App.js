2ea0503365c13f386768df7d0ddc3c0b2c583294
ryangiomi <ryangiomi@users.noreply.github.com>
2023-01-16 12:05:17 -0800

    Refactor directory + add some css (#1)
    * Update 8 files
    
    * Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./styles.css";
 
 import Rotation from "./Rotation";
-import personIndex from "./startRotation";
 
 export default function App() {
   return (
     <div className="App">
-      <Rotation personIndex={personIndex} />
+      <Rotation />
     </div>
   );
 }
---
+++ src/Rotation/Rotation.jsx
@@ -0,0 +1,77 @@
+import personIndex from "./startRotation";
+
+const person = ["Evelette", "Ashlyn", "Juliette", "Keilyn", "Ronnie"];
+
+const time = [
+  "0600",
+  "0700",
+  "0800",
+  "0900",
+  "1000",
+  "1100",
+  "1200",
+  "1300",
+  "1400",
+  "1500",
+  "1600",
+  "1700",
+  "1800",
+  "1900",
+  "2000",
+  "2100",
+  "2200",
+  "2300",
+];
+
+export default (props) => {
+  let currentPersonIndex = props.personIndex || personIndex;
+
+  return time.map((time, index) => {
+    currentPersonIndex = currentPersonIndex % 5;
+    currentPersonIndex++;
+
+    return (
+      <div
+        className="rotation-grid"
+        key={index}
+        style={{
+          display: "grid",
+          gridRowStart: 1,
+          gridRowEnd: 2,
+          gridColumnStart: 1,
+          gridColumnEnd: 3,
+          height: "2rem",
+          width: "200px",
+        }}
+      >
+        <span
+          style={{
+            display: "grid",
+            gridRowStart: 1,
+            gridRowEnd: 2,
+            gridColumnStart: 1,
+            gridColumnEnd: 2,
+            paddingTop: ".4rem",
+            width: "100px",
+          }}
+        >
+          {time}
+        </span>
+        <span
+          style={{
+            display: "grid",
+            gridRowStart: 1,
+            gridRowEnd: 2,
+            gridColumnStart: 2,
+            gridColumnEnd: 3,
+            paddingTop: ".4rem",
+            justifyContent: "left",
+            width: "100px",
+          }}
+        >
+          {person[currentPersonIndex - 1]}
+        </span>
+      </div>
+    );
+  });
+};
